efb383ffeaaaee5fadedc1846d89cdab541a71b4
yazanash <yazan.ash.doonaas@gmail.com>
2026-01-23 14:25:02 +0300

    add robots file

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://uniceps.trio-verse.com/sitemap.xml",
+  };
+}
