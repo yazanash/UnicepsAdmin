738fa752aa55e36f191013be341d5585feec2465
yazanash <yazan.ash.doonaas@gmail.com>
2026-01-23 15:45:52 +0300

    disallow admin pages from robots

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
+        disallow: "/admin",
       },
     ],
     sitemap: "https://uniceps.trio-verse.com/sitemap.xml",
